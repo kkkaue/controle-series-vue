0fa18b485185a9df4ee2c6eab8dbd5d7ca80a38a
Kauê de Magalhães Brandão <kauedemagalhaes27@gmail.com>
2023-12-28 12:05:02 -0300

    feat: adiciona SeriesController e rota para listagem de series
    - Cria SeriesController.php
    - Cria rota '/series' para listagem de series

--- routes/web.php
+++ routes/web.php
@@ -35,4 +35,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/series', [App\Http\Controllers\SeriesController::class, 'index'])->name('series.index');
+
 require __DIR__.'/auth.php';
