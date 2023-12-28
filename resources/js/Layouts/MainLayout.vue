ef089ec60ca3bc7c04ff7b7051a76acff63bfac8
Kauê de Magalhães Brandão <kauedemagalhaes27@gmail.com>
2023-12-28 13:28:35 -0300

    feat(view): cria layout principal e atualiza página de séries
    - Adiciona novo layout em `resources/js/Layouts/MainLayout.vue`
    - Atualiza página de séries em `resources/js/Pages/Series/Index.vue`

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <Head :title="title" />
+  <h1>
+    {{ title }}
+  </h1>
+  <slot />
+</template>
+
+<script setup>
+import { Head } from '@inertiajs/vue3';
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+});
+</script>
--- resources/js/Pages/Series/Index.vue
+++ resources/js/Pages/Series/Index.vue
@@ -1,15 +1,15 @@
 <template>
-  <Head title="Series" />
-  <ul>
-    <li v-for="(serie, index) in series" :key="index">
-      {{ serie }}
-    </li>
-  </ul>
+  <MainLayout title="Series">
+    <ul>
+      <li v-for="(serie, index) in series" :key="index">
+        {{ serie }}
+      </li>
+    </ul>
+  </MainLayout>
 </template>
 
 <script setup>
-import { Head } from '@inertiajs/vue3';
-
+import MainLayout from '@/Layouts/MainLayout.vue';
 const props = defineProps({
   series: {
     type: Array,
