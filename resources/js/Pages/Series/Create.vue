3745dbd2063985dd0981f9cc584c1356aeefa0d2
Kauê de Magalhães Brandão <kauedemagalhaes27@gmail.com>
2024-01-02 10:34:47 -0300

    refactor: modifica o formulário de cadastro de séries - adiciona os campos 'Número de temporadas' e 'Episódios por temporada'

--- resources/js/Pages/Series/Create.vue
+++ resources/js/Pages/Series/Create.vue
@@ -4,10 +4,26 @@
       Voltar
     </ButtonLink>
     <form @submit.prevent="form.post(route('series.store'))" class="mt-4">
-      <label for="name" class="block mb-2">
-        Título da Série:
-      </label>
-      <input v-model="form.title" type="text" name="title" id="title" class="border border-gray-400 p-2 rounded w-full" />
+      <div class="grid grid-cols-12 gap-4">
+        <div class="col-span-12 md:col-span-8">
+          <label for="title" class="block mb-2">
+            Título da Série:
+          </label>
+          <input v-model="form.title" type="text" name="title" id="title" class="border border-gray-400 p-2 rounded w-full" />
+        </div>
+        <div class="col-span-12 md:col-span-2">
+          <label for="seasons" class="block mb-2">
+            Número de temporadas:
+          </label>
+          <input v-model="form.seasons" type="text" name="seasons" id="seasons" class="border border-gray-400 p-2 rounded w-full" />
+        </div>
+        <div class="col-span-12 md:col-span-2">
+          <label for="episodes" class="block mb-2">
+            Episódios por temporada:
+          </label>
+          <input v-model="form.episodes" type="text" name="episodes" id="episodes" class="border border-gray-400 p-2 rounded w-full" />
+        </div>
+      </div>
       <button type="submit" class="bg-gray-800 text-white rounded py-2 px-4 mt-4 hover:bg-gray-900">
         Salvar
       </button>
@@ -23,5 +39,7 @@ import { useForm } from '@inertiajs/vue3';
 
 const form = useForm({
   title: '',
+  seasons: '',
+  episodes: '',
 });
 </script>
